dc8a74ffcddb6cebbde20dd6d4fda12486a5ef15
Thiago C. S. Tioma <thgsouza@outlook.com>
2023-05-24 17:45:43 -0300

    feat: separating into components and sign-in page

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="https://github.com/souzathg"
+        target="_blank"
+        rel="noreferrer"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        @souzathg
+      </a>{' '}
+      na NLW da{' '}
+      <a
+        href="https://rocketseat.com.br"
+        className="underline transition-colors hover:text-gray-100"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
