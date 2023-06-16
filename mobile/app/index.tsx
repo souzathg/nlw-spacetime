14ec65546fb828b3306a65ed5c42a1b5fdb835f9
Thiago C. S. Tioma <thgsouza@outlook.com>
2023-06-16 17:21:00 -0300

    feat: mobile new memory page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,26 +1,11 @@
 import React, { useEffect } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { styled } from 'nativewind'
 import * as SecureStore from 'expo-secure-store'
-import { useRouter } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { api } from '../src/lib/api'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -32,12 +17,7 @@ const discovery = {
 export default function App() {
   const router = useRouter()
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
+  // eslint-disable-next-line no-unused-vars
   const [request, response, signInWithGithub] = useAuthRequest(
     {
       clientId: '6c77ccff6e4f9ecf7ab8',
@@ -48,6 +28,19 @@ export default function App() {
     },
     discovery,
   )
+
+  useEffect(() => {
+    /*     console.log(
+      makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }), */
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleOauthGithubCode(code)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [response])
 
   async function handleOauthGithubCode(code) {
     const response = await api.post('/register', {
@@ -60,32 +53,11 @@ export default function App() {
     router.push('/memories')
   }
 
-  useEffect(() => {
-    /*     console.log(
-      makeRedirectUri({
-        scheme: 'nlwspacetime',
-      }), */
-
-    if (response?.type === 'success') {
-      const { code } = response.params
-      handleOauthGithubCode(code)
-    }
-  }, [response])
-
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
+
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua capsula do tempo
@@ -108,9 +80,15 @@ export default function App() {
       </View>
 
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
-        Feito com 💜 pelo no NLW da Rocketseat{' '}
+        Com 💜 por{' '}
+        <Link href={`https://github.com/souzathg`} className="underline">
+          @souzathg
+        </Link>{' '}
+        &{' '}
+        <Link href={`http://rocketseat.com.br`} className="underline">
+          Rocketseat
+        </Link>{' '}
       </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
